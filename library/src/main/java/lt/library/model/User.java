0ac8c4baa49cdacc5863e2ba1db1aeb55e136f2a
Marius <marius.vinogradovas@stud.vtmc.lt>
2021-04-30 00:19:00 +0300

    Added takeABook

--- library/src/main/java/lt/library/model/User.java
+++ library/src/main/java/lt/library/model/User.java
@@ -1,5 +1,31 @@
 package lt.library.model;
 
+import java.util.List;
+
 public class User {
+
+	private Long id;
+	private List<Book> userBooks;
+
+	public User(Long id, List<Book> userBooks) {
+		this.id = id;
+		this.userBooks = userBooks;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Book> getUserBooks() {
+		return userBooks;
+	}
+
+	public void setUserBooks(List<Book> userBooks) {
+		this.userBooks = userBooks;
+	}
 
 }
